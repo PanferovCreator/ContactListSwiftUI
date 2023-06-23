9ffd79d2bdfe2717efee0967feb08c22cfabfae6
Panferov.creator <panferov.creator@gmail.com>
2023-06-23 16:27:57 +0500

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Dmitriy Panferov on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
